09105a8c57c9ae9049b12b356aeb8da704dd553a
Tommy Hsieh <tommy_hsieh@gss.com.tw>
2026-03-05 21:44:05 +0800

    feat: add hero section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { HeroSection } from "@/components/home/hero/hero-section";
+
 export default function Home() {
-  return <main className="min-h-screen bg-gray-50"></main>;
+  return (
+    <main className="min-h-screen bg-gray-50 w-full flex flex-col">
+      <HeroSection />
+    </main>
+  );
 }
---
+++ components/home/hero/hero-title.tsx
@@ -0,0 +1,13 @@
+export function HeroTitle() {
+  return (
+    <>
+      <h2 className="mb-4 text-4xl font-black tracking-tight text-foreground md:text-6xl">
+        Find your next <span className="text-primary">favorite meal.</span>
+      </h2>
+      <p className="mb-10 text-lg text-muted-foreground font-medium max-w-2xl px-4">
+        Discover thousands of high-quality recipes for every occasion, curated
+        by professional chefs.
+      </p>
+    </>
+  );
+}
